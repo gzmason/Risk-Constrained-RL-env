a4d911142bc3421130174d33ac6619823182b782
Ruilin Liu <ruilinli@usc.edu>
2019-09-08 16:47:45 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='Risk-Constrained-RL_env',
+setup(name='rl_env',
       version='0.0.1',
       install_requires=['gym','pandas','numpy']  # And any other dependencies foo needs
 )
